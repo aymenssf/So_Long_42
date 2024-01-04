045ae2d2e4b0cc1049f3d4d4c8fd9741cefb77c2
Aymen ASSAF <aymen.assaf@edu.uca.ma>
2024-01-04 18:59:05 +0100

    add more functions

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: aassaf <aassaf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 02:42:08 by aassaf            #+#    #+#             */
-/*   Updated: 2024/01/03 22:19:15 by aassaf           ###   ########.fr       */
+/*   Updated: 2024/01/04 18:56:43 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # include "libft/libft.h"
 # include "gnl/get_next_line.h"
 # include <mlx.h>
-
+# include <stdio.h>
 typedef struct s_player
 {
 	int x;
@@ -47,6 +47,7 @@ typedef struct s_map
 	t_image *wall;
 	t_image *empty;
 	t_image *exit;
+	int fd;
 	int row;
 	int col;
 	char **arr_map;
@@ -54,13 +55,15 @@ typedef struct s_map
 
 
 void    validate_map(t_map *map);
-void read_map(char *av, t_map);
 int parse_wall(t_map *map, int i, int j);
 void    *ft_realloc(void *p, size_t new_size, size_t old_size);
+void	read_map(int ac, char **av, t_map *map, int j);
 int     print_error(char *s);
 char	*get_next_line(int fd);
 void    check_arg(int ac, char **av);
 void init_struct(t_map *map);
+char	**duplicate_map(t_map *map);
+int     parse_exit_player(t_map *map);
 
 // void    validate_and_init_game(char *mymap, t_map map, int i);
 // void    check_arg(int ac, char **av, t_map *map);
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: aassaf <aassaf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 02:41:05 by aassaf            #+#    #+#             */
-/*   Updated: 2023/12/31 02:41:05 by aassaf           ###   ########.fr       */
+/*   Updated: 2024/01/04 12:38:05 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,24 @@ void *ft_realloc(void *p, size_t new_size, size_t old_size)
         ft_memcpy(new_p, p, old_size);
         free(p);
         return (new_p);
+}
+
+void    count_line(t_map *map)
+{
+        char *buff;
+
+        buff = get_next_line(map->fd);
+        if(buff == 0)
+                print_error("Error in map");
+        map->col = ft_strlen(buff);
+        while(buff != NULL)
+        {
+                free(buff);
+                map->row++;
+                buff = get_next_line(map->fd);
+                if(buff == NULL)
+                        free(buff);
+        }
 }
 
 int     print_error(char *s)
